cf1e923b7600acdfa651dce5a7de1090f27bb081
Salma Oualla <salmaoualla@gmail.com>
2023-04-02 04:11:24 -0700

    digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Discription: 'all single digit numbers of base 10 starting from 0'
- * Ruturn: Always 0
+ * Description: 'all single digit numbers of base 10 starting from 0'
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + 48);
 	}
